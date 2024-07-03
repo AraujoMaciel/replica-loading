b6c27f0ddbbaa9d68a9bb68e1da3470086e404ae
Maciel <macielsemr@gmail.com>
2024-07-03 15:39:57 -0300

    feat: iniciando projeto

---
+++ app/paginas/sobre/page.tsx
@@ -0,0 +1,9 @@
+import { montserrat } from "@/app/fontes"
+
+export default function Sobre(){
+    return(
+        <div>
+            <div className={`montserrat.className`}>Sobre</div>
+        </div>
+    )
+}
