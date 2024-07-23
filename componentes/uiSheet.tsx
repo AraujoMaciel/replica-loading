8c58c1aed2fcaa1084839d95194093f82ea5cb0f
Maciel <macielsemr@gmail.com>
2024-07-23 11:08:57 -0300

    fix:corrigindo menu Sheet para fechar automaticamente ao alternar entre páginas

--- componentes/uiSheet.tsx
+++ componentes/uiSheet.tsx
@@ -1,3 +1,5 @@
+'use-client'
+
 import {
     Sheet,
     SheetClose,
@@ -15,21 +17,27 @@ import {BiLogoGmail} from "react-icons/bi"
 import { TiSocialLinkedin } from "react-icons/ti";
 import { IoLogoInstagram } from "react-icons/io";
 import { montserrat } from "@/app/fontes";
+import { useState } from "react";
 
 export default function UiSheet(){
+
+    const [menu, setMenu] = useState<boolean>(false);
+
     return(
       <Sheet>
-      <SheetTrigger>
+      <SheetTrigger onClick={()=>{setMenu(true)}}>
         <HiOutlineMenuAlt3 
           className="text-2xl hover:cursor-pointer"
         />
       </SheetTrigger>
-      <SheetContent className={`bg-roxo text-white border-none pt-20`}>
+      <SheetContent className={`bg-roxo text-white border-none pt-20 ${menu?'visible':'hidden'}`}>
         <SheetHeader className={`${montserrat.className} font-medium`}>
           <SheetTitle className="flex flex-col gap-7 text-white">
-            <Link href="/paginas/sobre">Sobre a Loading</Link>
+
+              <a onClick={()=>{setMenu(false)}} href="/paginas/sobre">Sobre a Loading</a>
             
-            <Link href="/paginas/portifolio">Portifólio</Link>
+              <a onClick={()=>{setMenu(false)}} href="/paginas/portifolio">Portifólio</a>
+            
           </SheetTitle>
           <SheetDescription className="absolute bottom-4 w-full left-0">
             <div className="flex gap-4 justify-center items-center text-3xl text-white">
